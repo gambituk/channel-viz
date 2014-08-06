1704de5b1621c7e1a1ee7dfca94cff9c3ede3b03
gambituk <gambituk@hotmail.com>
2014-08-06 10:23:52 +0200

    Update follows.js
    added default key etc

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= '3Gn4uAkSsngAStkVKM3Kn7vAPJaaTpYWFgD5X8O0CZERvqyX', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [403785305], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Gambits Feed', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		dataColor		= 'FF0066', // CSS HEX value of color to represent data (omit leading #)
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
